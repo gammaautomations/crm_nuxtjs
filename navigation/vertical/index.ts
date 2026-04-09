35e9e932cccacbba0ae2b45f01bfec0eca7b478d
fmateosc <fmateosc@gmail.com>
2026-04-09 22:54:09 +0100

    feat: update navigation menu

--- navigation/vertical/index.ts
+++ navigation/vertical/index.ts
@@ -1,12 +1,49 @@
 export default [
   {
-    title: 'Home',
+    title: 'Dashboard',
     to: { name: 'index' },
     icon: { icon: 'tabler-smart-home' },
   },
   {
-    title: 'Configuración',
-    to: { name: 'admin' },
-    icon: { icon: 'tabler-file' },
+    title: 'Leads',
+    icon: { icon: 'tabler-users' },
+    children: [
+      {
+        title: 'Lista',
+        to: { name: 'leads' },
+        icon: { icon: 'tabler-list' },
+      },
+      {
+        title: 'Kanban',
+        to: { name: 'leads-kanban' },
+        icon: { icon: 'tabler-layout-kanban' },
+      },
+    ],
+  },
+  {
+    title: 'Contactos',
+    to: { name: 'contacts' },
+    icon: { icon: 'tabler-address-book' },
+  },
+  {
+    title: 'Administración',
+    icon: { icon: 'tabler-settings' },
+    children: [
+      {
+        title: 'Abogados',
+        to: { name: 'admin-lawyers' },
+        icon: { icon: 'tabler-briefcase' },
+      },
+      {
+        title: 'Especialidades',
+        to: { name: 'admin-specialties' },
+        icon: { icon: 'tabler-certificate' },
+      },
+      {
+        title: 'Configuración',
+        to: { name: 'admin' },
+        icon: { icon: 'tabler-adjustments' },
+      },
+    ],
   },
 ]
